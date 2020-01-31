62a93a8df7d5b55fa4a78a7fe77574052f12b941
alfredo <alfredtito97@gmail.com>
2020-01-31 18:04:38 -0200

    consertando homepage - projeto krilow

---
+++ projects/krilow/precache-manifest.7f9e76fa2af9f9cdfbbb07b2a4c0c028.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c527e0b8c12993ef0ad49472faa5dae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b7eafd80c07e4f475418",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "031f9ee3e81bc335d5fb",
+    "url": "./static/js/2.36b1a063.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "./static/js/2.36b1a063.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b7248bf6c695a64eaa31",
+    "url": "./static/js/3.386bddc7.chunk.js"
+  },
+  {
+    "revision": "d981e74d1ed6e0753d6a",
+    "url": "./static/js/4.e095a981.chunk.js"
+  },
+  {
+    "revision": "b7eafd80c07e4f475418",
+    "url": "./static/js/main.3ef12365.chunk.js"
+  },
+  {
+    "revision": "25f50987ebb5bd31f599",
+    "url": "./static/js/runtime-main.8a9f17b0.js"
+  }
+]);
--- projects/krilow/service-worker.js
+++ projects/krilow/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.27602694384e29298c40c07e0552ee57.js"
+  "./precache-manifest.7f9e76fa2af9f9cdfbbb07b2a4c0c028.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
