32b94f608b2e25b1db1eda2521566dca264a5c59
alfredo <tito.alfred@hotmail.com>
2020-07-20 11:02:24 -0300

    adicionando projeto para apresentação

---
+++ projects/qr-code-reader/precache-manifest.44efd77dd64337375d5fb3a4afc1b90d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba9b4de1dbdcf3205154a2c8b9a8cf26",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b2cceb497fa8a7bc7ec8",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1ad8c8f936da257a022c",
+    "url": "./static/js/2.d968dc0a.chunk.js"
+  },
+  {
+    "revision": "ea1d409d48cb0a80fc4d61530631d4c9",
+    "url": "./static/js/2.d968dc0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2cceb497fa8a7bc7ec8",
+    "url": "./static/js/main.43133230.chunk.js"
+  },
+  {
+    "revision": "ff74703d435b60fcef60",
+    "url": "./static/js/runtime-main.24cda366.js"
+  }
+]);
